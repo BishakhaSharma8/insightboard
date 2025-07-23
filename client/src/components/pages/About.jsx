6305bf65ca6544c1570873d7784344de7702a3e5
Bishakha Sharma <bishakhasharma8@gmail.com>
2025-07-23 22:33:16 +0530

    Updated About page for Netlify

--- client/src/components/pages/About.jsx
+++ client/src/components/pages/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
       <div style={styles.container}>
         <h1 style={styles.heading}>About Me</h1>
         <p style={styles.text}>
-          👋 Hello! I'm <strong>Bishakha Sharma</strong>, a highly motivated <strong>Java Developer</strong> and <strong>MERN Stack Engineer</strong> with a passion for building scalable and user-centric web applications.
+          👋 Hello! I'm <strong>Bishakha Sharma</strong>, a highly motivated <strong>Java Developer</strong> and <strong>MERN Stack Developer</strong> with a passion for building scalable and user-centric web applications.
         </p>
         <p style={styles.text}>
           With hands-on experience in technologies like <strong>Java, Spring Boot, MongoDB, Express.js, React.js, and Node.js</strong>, I enjoy crafting intuitive front-end interfaces and architecting powerful back-end systems. My strength lies in transforming ideas into functional and aesthetic digital products.
